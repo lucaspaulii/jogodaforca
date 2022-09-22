b6eb4566ca0770ef2c8aef13e17a1db11adf611a
Lucas C. Pauli <lucaspauli22@gmail.com>
2022-09-22 16:17:42 -0300

    image changing (also with delay bug)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,15 @@
 import forca0 from "../images/forca0.png";
-import forca1 from "../images/forca0.png";
-import forca2 from "../images/forca0.png";
-import forca3 from "../images/forca0.png";
-import forca4 from "../images/forca0.png";
-import forca5 from "../images/forca0.png";
-import forca6 from "../images/forca0.png";
+import forca1 from "../images/forca1.png";
+import forca2 from "../images/forca2.png";
+import forca3 from "../images/forca3.png";
+import forca4 from "../images/forca4.png";
+import forca5 from "../images/forca5.png";
+import forca6 from "../images/forca6.png";
 import palavras from "./palavras";
 import React, { useState } from "react";
 
 export default function App() {
+  const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
   const alfabeto = [
     "a",
     "b",
@@ -51,6 +52,7 @@ export default function App() {
         displayLetter(letter, word);
       } else {
         setLetrasErradas([...letrasErradas, letter]);
+        setImagem(images[letrasErradas.length]);
       }
     }
   }
